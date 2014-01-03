c827b407c5254c4411cab3b24966efd2e6deee3d
John Lusby <jlusby42@gmail.com>
2014-01-03 14:54:00 -0800

    convert monitor notifier to module

--- MonitorNotifier.py
+++ MonitorNotifier.py
@@ -2,17 +2,14 @@
 
 import smtplib
 from email.mime.text import MIMEText
+def notify(from_addr, to_addrs, subject, body, mail_server)
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = from_addr
+    msg['To'] = ', '.join(to_addrs)
 
-me = 'support@asaiatm.com'
-you = 'gus@asaiatm.com'
-
-msg = MIMEText("This is a test email sent via python for use with the asai monitor program")
-msg['Subject'] = "Monitor Notifications Test Email"
-msg['From'] = me
-msg['To'] = you
-
-server = smtplib.SMTP('mail.asaiatm.com:587')
-server.ehlo()
-server.starttls()
-server.sendmail(me, [you], msg.as_string())
-server.quit()
+    server = smtplib.SMTP(mail_server)
+    server.ehlo()
+    server.starttls()
+    server.sendmail(from_addr, to_addrs, msg.as_string())
+    server.quit()
